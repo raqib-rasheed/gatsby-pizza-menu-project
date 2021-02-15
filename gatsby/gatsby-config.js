1ff2453761bd1f88bd82510b4b09fd10ebf8eabf
raqib-rasheed <raqib.abdulrasheed@outlook.com>
2021-02-15 19:19:22 +0530

    aahs

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,25 +1,24 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
 export default {
-  pathPrefix: '/pizza',
+  pathPrefix: "/pizza",
   siteMetadata: {
     title: `Slicks Slices`,
-    siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place in Hamilton!',
-    twitter: '@slicksSlices',
+    siteUrl: "https://gatsby.pizza",
+    description: "The best pizza place in Hamilton!",
+    twitter: "@slicksSlices",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
       // this is the name of the plugin you are adding
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
-        projectId: '0jfvvkkd',
-        dataset: 'production',
-        watchMode: true,
+        projectId: "0jfvvkkd",
+        dataset: "production",
         token: process.env.SANITY_TOKEN,
       },
     },
